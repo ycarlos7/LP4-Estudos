afd5f3436789ca04f38ff5d92b470bb454c690e8
Yan <ycarlospereira@gmail.com>
2025-08-14 20:02:00 -0300

    Primeira parte da aula do dia 14/08

---
+++ Aulas/Aula_14_08/exemplo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+   $dia = date("d"); 
+?>
+<h1> Hoje é dia <?= $dia ?> de Agosto de 2025 <h1>
+    
+</body>
+</html>
