d127b66558cb91076dd9fd722b58e5db33349c6c
Yan <ycarlospereira@gmail.com>
2025-08-28 21:46:39 -0300

    correção do exercicio 19 da lista 1, criação da lista 2 e realização do exercicio 5 da lista 2

---
+++ lista_2/cabecalho.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Lista de exercícios 2</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">Lista de exercícios 2</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+<li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" id="dropdown1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Exercícios
+  </a>
+  <ul class="dropdown-menu" aria-labelledby="dropdown1">
+    <li><a class="dropdown-item" href="exercicio1.php">Exercício 1</a></li>
+<li><a class="dropdown-item" href="exercicio2.php">Exercício 2</a></li>
+<li><a class="dropdown-item" href="exercicio3.php">Exercício 3</a></li>
+<li><a class="dropdown-item" href="exercicio4.php">Exercício 4</a></li>
+<li><a class="dropdown-item" href="exercicio5.php">Exercício 5</a></li>
+<li><a class="dropdown-item" href="exercicio6.php">Exercício 6</a></li>
+<li><a class="dropdown-item" href="exercicio7.php">Exercício 7</a></li>
+<li><a class="dropdown-item" href="exercicio8.php">Exercício 8</a></li>
+<li><a class="dropdown-item" href="exercicio9.php">Exercício 9</a></li>
+<li><a class="dropdown-item" href="exercicio10.php">Exercício 10</a></li>
+  </ul>
+</li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container py-3">
---
+++ lista_2/exercicio5.php
@@ -0,0 +1,38 @@
+<?php
+
+include("cabecalho.php");
+
+$mes = 4;
+switch ($mes) {
+    case 1:
+        echo "Janeiro";
+        break;
+    case 2:
+        echo "Fevereiro";
+        break;
+    case 3:
+        echo "Março";
+        break;
+    case 4:
+        echo "Abril";
+        break;
+    case 5:
+        echo "Maio";
+        break;
+    case 6:
+        echo "Junho";
+        break;
+    case 7:
+        echo "Julho";
+        break;
+    case 8:
+        echo "Agosto";
+        break;
+    case 9:
+        echo "Setembro";
+        break;
+    default:
+        echo " Pode ser Outubro, Novembro ou Dezembro";
+}
+
+include("rodape.php");
